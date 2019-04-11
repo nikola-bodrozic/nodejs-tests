3361ffbf44c30aed65ba08bef7f8994de2a4db06
Your Name <you@example.com>
2019-04-11 22:40:45 +0200

    testing promise and class

--- test/actions.test.js
+++ test/actions.test.js
@@ -10,6 +10,37 @@ const UsersDao = require('../src/usersDao');
 
 let userDaoStub
 
+lab.experiment("Class Users", () => {
+  lab.beforeEach(() => {
+    userDaoStub = sinon.stub(UsersDao.prototype, 'getUserLocation')
+  });
+
+  lab.afterEach(() => {
+    userDaoStub.restore()
+  });
+
+  lab.test("should stub userDao.getUser() with arg 5 and return object", () => {
+  	  userDaoStub.returns({ lat: '-1.3159', lng: '43.1496' })
+      const val = actions.showUser(5)
+      expect(val).to.equal({ lat: '-1.3159', lng: '43.1496' })
+      expect(userDaoStub.getCall(0).args[0]).to.equal(5)
+      expect(userDaoStub.calledOnce).to.be.equal(true);
+  });
+
+  lab.test("should stub userDao.getUser() with fake function", () => {  
+    userDaoStub.callsFake (() => 'bar');
+    const val = actions.showUser(7)
+    expect(val).to.equal('bar')
+  });
+
+  lab.test("should stub userDao.getUser() with arg = 48", () => {  
+    userDaoStub.withArgs(45).returns('some arg');
+    userDaoStub.withArgs(48).returns('some other arg');
+    const val = actions.showUser(48)
+    expect(val).to.equal('some other arg')
+  });
+});
+
 lab.experiment("Promises", ()=>{
   lab.test("should resolve promise to `resolved` string", async () => {
     expect(await actions.handlePromise(0.7)).to.be.equal("resolved");
@@ -18,33 +49,3 @@ lab.experiment("Promises", ()=>{
     expect(await actions.handlePromise(0.3)).to.be.equal("rejected");
   });
 })
-
-lab.experiment("Class Users", () => {
-
-  lab.test("should stub userDao.getUser()", async () => {
-    actions.showUser()
-    
-	  userDaoStub = sinon.stub(UsersDao.prototype, 'getUserLocation').returns({ lat: '-1.3159', lng: '43.1496' })
-    const val1 = actions.showUser(5)
-    expect(val1).to.equal({ lat: '-1.3159', lng: '43.1496' })
-    expect(userDaoStub.getCall(0).args[0]).to.be.a.number();
-    expect(userDaoStub.getCall(0).args[0]).to.equal(5)
-    expect(userDaoStub.calledOnce).to.be.equal(true);
-    userDaoStub.restore()
-
-    userDaoStub = sinon.stub(UsersDao.prototype, 'getUserLocation').callsFake (() => 'bar');
-    const val2 = actions.showUser(7)
-    expect(val2).to.equal('bar')
-    expect(userDaoStub.calledOnce).to.be.equal(true);
-    userDaoStub.restore()
-
-    userDaoStub = sinon.stub(UsersDao.prototype, 'getUserLocation')
-    userDaoStub.withArgs(45).returns('some arg');
-    userDaoStub.withArgs(48).returns('some other arg');
-    const val3 = actions.showUser(48)
-    expect(val3).to.equal('some other arg')
-    expect(userDaoStub.calledOnce).to.be.equal(true);
-	  userDaoStub.restore()
-  });
-
-});
